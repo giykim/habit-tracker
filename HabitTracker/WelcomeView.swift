a5917e9dd013fbfcfc8acf9a490a9b268d4b8267
Giyoung Kim <gk122@duke.edu>
2026-02-03 15:54:50 -0600

    Add design to welcome view

--- HabitTracker/WelcomeView.swift
+++ HabitTracker/WelcomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WelcomeView.swift
 //  HabitTracker
 //
 //  Created by Giyoung Kim on 2/3/26.
@@ -9,17 +9,49 @@ import SwiftUI
 import SwiftData
 
 struct WelcomeView: View {
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+
     var body: some View {
         ZStack {
             // MARK: Background
+            ZStack {
+                Circle()
+                    .foregroundStyle(.cyan)
+                    .frame(width: screenWidth / 1.3)
+                    .offset(
+                        x: -screenWidth / 2,
+                        y: -screenHeight / 2
+                    )
+
+                Circle()
+                    .foregroundStyle(.green)
+                    .frame(width: screenWidth / 1.1)
+                    .offset(
+                        x: screenWidth / 3,
+                        y: screenHeight / 3
+                    )
+                
+                Circle()
+                    .foregroundStyle(.yellow)
+                    .frame(width: screenWidth / 1.8)
+                    .offset(
+                        x: screenWidth / 4,
+                        y: -screenHeight / 4
+                    )
+            }
             
             // MARK: Content
             VStack (alignment: .leading, spacing: 15) {
                 // App Name
-                Text("Habit Tracker")
+                Text("HABIT TRACKER")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
                 
                 // Slogan
-                Text("Maintain your daily and ongoing schedule")
+                Text("Maintain your daily habits and long-term goals")
+                    .font(.title2)
+                    .fontWeight(.semibold)
                 
                 // Get Started Button
                 Button(
@@ -28,9 +60,15 @@ struct WelcomeView: View {
                     },
                     label: {
                         Text("Start")
+                            .foregroundStyle(.white)
+                            .padding()
+                            .fontWeight(.bold)
+                            .background(.orange)
+                            .cornerRadius(10)
                     }
                 )
             }
+            .padding()
         }
     }
 }
